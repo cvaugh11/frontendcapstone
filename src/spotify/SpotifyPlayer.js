8cfe741d3b2947d1be931e40cd183132ec208840
Carlton Vaughn <RichHomieVaughn@carltons-mbp.home>
2020-08-20 06:53:19 -0400

    frontend

---
+++ src/spotify/SpotifyPlayer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./SpotifyPlayer.css";
+
+const SpotifyPlayer = props => {
+  
+  return (
+    <div className="songRow">
+        <img className="songRow_album" src={props.item.album.images[0].url}/>
+        <div className="songRow_info">
+        <h3>{props.item.name}</h3>
+        <p> By: {props.item.artists[0].name}</p>
+        </div>
+    </div>
+  );
+}
+
+export default SpotifyPlayer;
